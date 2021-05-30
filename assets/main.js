2667b0f721806073d27f6a6c8a88b2afe6fea6b8
Divyanth2468 <80347640+Divyanth2468@users.noreply.github.com>
2021-05-30 11:44:09 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     var headerHeight = $('header').outerHeight();
-    $(".slide-section").click(function(e){
+    $(".slide-section").click(function(){
         var linkHref = $(this).attr("href");
         $("html, body").animate({
-            scrollTop: $(linkHref).offset().top
+            scrollTop: $(linkHref).offset().top - (headerHeight*4.7)
         }, 1000)
-        e.preventDefault();
     })
 })
