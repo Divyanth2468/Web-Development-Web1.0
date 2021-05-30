2bc97582d712cf21232f8109affa02cb8e062a18
Divyanth2468 <80347640+Divyanth2468@users.noreply.github.com>
2021-05-30 12:17:36 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,16 @@ $(document).ready(function(){
         }, 1000)
     })
 })
+
+if($(window).width() < 767)
+{
+    $(document).ready(function(){
+        var headerHeight = $('header').outerHeight();
+        $(".slide-section").click(function(){
+            var linkHref = $(this).attr("href");
+            $("html, body").animate({
+                scrollTop: $(linkHref).offset().top - (headerHeight*5.2)
+            }, 1000)
+        })
+    })  
+}
